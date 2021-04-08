082b6197ca68d967589134536de2487d62c1ebe0
jucic1 <julia.cicha60@gmail.com>
2021-04-08 19:25:51 +0200

    initial commit

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Domains d = new Domains();
         d.initColorsDomain(new ArrayList<String>(Arrays.asList("Yellow", "Blue", "Green", "White", "Red")));
+        d.initNationalitiesDomain(new ArrayList<String>(Arrays.asList("English", "German", "Swedish", "Norwegian", "Danish")));
+        d.initCigarettesDomain(new ArrayList<String>(Arrays.asList("Filterless", "Pipe", "Cigar", "Menthol", "Light")));
+        d.initDrinksDomain(new ArrayList<String>(Arrays.asList("Coffee", "Water", "Tee", "Milk", "Beer")));
+        d.initPetsDomain(new ArrayList<String>(Arrays.asList("Horse", "Fish", "Bird", "Dog", "Cat")));
         Utils.printArrayOfArrays(d.getColorsDomain());
     }
 }
